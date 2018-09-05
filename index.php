a07b0a546fb419898506a9ea6f6151b7ff52e710
Everton Macedo <evertonsmacedo@hotmail.com>
2018-09-05 15:51:19 -0300

    Criando novo usuario

--- index.php
+++ index.php
@@ -8,19 +8,44 @@ require_once("config.php");
 //$root->loadById(3);
 //echo $root;
 
+
+
 //Carrega uma lista de usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
+
+
 
 //Carrega uma lista de usuários buscando pelo login
 //$search = Usuario::search("jo");
 //echo json_encode($search);
 
+
+
 //Carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("jose","123456");
+//echo $usuario;
+
+
+//Criando um novo usuario
+//$aluno = new Usuario("aluno","qwert");
+//$aluno->insert();
+//echo $aluno;
+
+
 $usuario = new Usuario();
-$usuario->login("jose","123456");
+$usuario->loadById(8);
+
+$usuario->update("professor", "!@#$%");
 
 echo $usuario;
+
+
+
+
+
+
 
 
  ?>
